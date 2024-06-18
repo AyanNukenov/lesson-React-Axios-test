f606c74bf4f984dbbc98ef0aff0d2dddd648c773
AyanNukenov <165056743+AyanNukenov@users.noreply.github.com>
2024-06-18 23:08:55 +0500

    test (practice)

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setUsers(response.data);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        setError(error);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
